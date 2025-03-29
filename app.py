c6a966e5808d71b98a94c307a39bc5c1977f16c5
mahmoudnasser1561 <mahmoudnasser1561@gmail.com>
2025-03-29 22:34:38 +0200

    built 500 custom page

--- app.py
+++ app.py
@@ -15,3 +15,7 @@ def user(name):
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(e):
+    return render_template('500.html'), 500
