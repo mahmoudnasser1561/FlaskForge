66114938502a4ca8acf5c9d10ea6ac4578aa5ab7
mahmoudnasser1561 <mahmoudnasser1561@gmail.com>
2025-04-01 22:13:58 +0200

    made config.py

--- app.py
+++ app.py
@@ -15,18 +15,18 @@ from threading import Thread
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SECRET_KEY'] = 'hard to guess'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = 'FlaskForge'
-app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <mahmoudnasser1561@gmail.com>'
-app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
+# app.config['SQLALCHEMY_DATABASE_URI'] =\
+#     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SECRET_KEY'] = 'hard to guess'
+# app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+# app.config['MAIL_PORT'] = 587
+# app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+# app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+# app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = 'FlaskForge'
+# app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <mahmoudnasser1561@gmail.com>'
+# app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
