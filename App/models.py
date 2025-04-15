acace5f4158349c49e8e50e53279cc5f7287bf85
Mahmoud <mahmoudnasser1561@gmail.com>
2025-04-15 14:56:19 +0200

    built the foundation for Permissions and Role assignment

--- App/models.py
+++ App/models.py
@@ -6,14 +6,42 @@ from . import login_manager
 from . import db 
 
 
+class Permission:
+    FOLLOW = 1
+    COMMENT = 2
+    WRITE = 4
+    MODERATE = 8
+    ADMIN = 16
+
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
 
     def __repr__(self):
         return '<Role %r>' % self.name
+    
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
+
+    def has_permission(self, perm):
+        return self.permissions & perm == perm
+    
+    def reset_permissions(self):
+        self.permissions = 0
+
+    def remove_permission(self, perm):
+        if self.has_permission(perm):
+            self.permissions = 0
+
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
 
 
 class User(db.Model, UserMixin):
--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import os
 from flask_migrate import Migrate
 from App import create_app, db
-from App.models import User, Role
+from App.models import User, Role, Permission
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User, Role=Role)
+    return dict(db=db, User=User, Role=Role, Permission=Permission)
 
 @app.cli.command()
 def test():
