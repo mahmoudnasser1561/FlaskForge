09119477bab09d739518d0219d9523b336afdaca
Mahmoud <mahmoudnasser1561@gmail.com>
2025-09-26 02:29:17 +0300

    added Post model

--- App/main/views.py
+++ App/main/views.py
@@ -1,35 +1,25 @@
 from flask import render_template, session, redirect, url_for, current_app, flash
 from datetime import datetime
 from .. import db
-from ..models import User, Role
+from ..models import User, Role, Post, Permission
 from ..email import send_email
 from . import main
-from .forms import NameForm, EDitProfileForm, EditProfileAdminForm
+from .forms import NameForm, EDitProfileForm, EditProfileAdminForm, PostForm
 from flask_login import login_required, current_user
 from ..decorators import admin_required
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            db.session.commit()
-            session['known'] = False
-            if current_app.config['FLASKY_ADMIN']:
-                send_email(current_app.config['FLASKY_ADMIN'], 'New User',
-                           'mail/new_user', user=user)
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
+    form = PostForm()
+    if current_user.can(Permission.WRITE) and form.validate_on_submit():
+        post = Post(body=form.body.data,
+                    author=current_user._get_current_object())
+        db.session.add(post)
+        db.session.commit()
         return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False),
-                           current_time=datetime.utcnow())
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', form=form, posts=posts, current_time=datetime.utcnow())
 
 @main.route('/user/<username>')
 def user(username):
