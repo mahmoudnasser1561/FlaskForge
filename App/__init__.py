3271966b2fc149055e86437450c8474389b5464b
Mahmoud <mahmoudnasser1561@gmail.com>
2025-10-12 11:26:20 +0300

    added MarkDown style Text

--- App/__init__.py
+++ App/__init__.py
@@ -5,6 +5,7 @@ from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 from flask_login import LoginManager
+from flask_pagedown import PageDown
 
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
@@ -13,6 +14,7 @@ bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+page_down = PageDown()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -23,6 +25,7 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    page_down.init_app(app)
 
     from .main import main as main_blueprint
     from .auth import auth as auth_blueprint
