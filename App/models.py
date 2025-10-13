b38f963a3709852753b44985d53755c09962de98
Mahmoud <mahmoudnasser1561@gmail.com>
2025-10-13 04:08:58 +0300

    added post code sanitization

--- App/models.py
+++ App/models.py
@@ -6,6 +6,8 @@ from flask import current_app, request
 from . import login_manager
 from . import db 
 import hashlib
+from markdown import markdown
+import bleach
 
 
 class Permission:
@@ -170,6 +172,18 @@ class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     author = db.relationship('User', backref='posts')
+    
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags, strip=True))
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
