7bf751c750657b5bd4acfe332fc93809a9b4731c
Mahmoud <mahmoudnasser1561@gmail.com>
2025-11-14 14:26:05 +0200

    added the comment moderation base

--- App/main/views.py
+++ App/main/views.py
@@ -202,3 +202,15 @@ def show_followed():
     resp = make_response(redirect(url_for('.index')))
     resp.set_cookie('show_followed', '1', max_age=30*24*60*60)
     return resp
+
+@main.route('/moderate')
+@login_required
+@permission_required(Permission.MODERATE)
+def moderate():
+    page = request.args.get('page', 1, type=int)
+    pagination = Comment.query.order_by(Comment.timestamp.desc()).paginate(
+        page=page, per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'],
+        error_out=False)
+    comments = pagination.items
+    return render_template('moderate.html', comments=comments,
+                           pagination=pagination, page=page)
