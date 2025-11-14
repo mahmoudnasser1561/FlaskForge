3acf810fc1d391bcc87679d28c11fc7f0eef0fc1
Mahmoud <mahmoudnasser1561@gmail.com>
2025-11-14 11:29:10 +0200

    created CommentForm

--- App/main/forms.py
+++ App/main/forms.py
@@ -49,3 +49,6 @@ class PostForm(FlaskForm):
     body = PageDownField("What's on your mind?", validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+class CommentForm(FlaskForm):
+    body = StringField('', validators=[DataRequired()])
+    submit = SubmitField('Submit')
