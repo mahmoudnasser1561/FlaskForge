3bb590ff8b88c9be2f8470cf65bac73ee22dc6af
Mahmoud <mahmoudnasser1561@gmail.com>
2025-11-16 14:44:05 +0200

    updated config

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = 'Flask Forge'
-    FLASKY_MAIL_SENDER = os.environ.get('FLASKY_MAIL_SENDER', 'Flasky Admin <mahmoudnasser1561@gmail.com>')
+    FLASKY_MAIL_SENDER = os.environ.get('FLASKY_MAIL_SENDER')
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASKY_POSTS_PER_PAGE = 20
