d1d659439fd2f7ace6ebe81698e620d124383e16
Mahmoud <mahmoudnasser1561@gmail.com>
2026-06-27 20:28:01 +0300

    added Resource serialization to User and Post classes

--- App/models.py
+++ App/models.py
@@ -2,7 +2,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import URLSafeTimedSerializer as Serializer
 from flask_login import UserMixin, AnonymousUserMixin
 from datetime import datetime
-from flask import current_app, request
+from flask import current_app, request, url_for
+from App.exceptions import ValidationError
 from . import login_manager
 from . import db 
 import hashlib
@@ -214,6 +215,19 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return '<User %r>' % self.username
     
+    def to_json(self):
+        json_user = {
+            'url': url_for('api.get_user', id=self.id),
+            'username': self.username,
+            'member_since': self.member_since,
+            'last_seen': self.last_seen,
+            'posts_url': url_for('api.get_user_posts', id=self.id),
+            'followed_posts_url': url_for('api.get_user_followed_posts',
+                                          id=self.id),
+            'post_count': self.posts.count()
+        }
+        return json_user
+    
 
 class AnonymousUser(AnonymousUserMixin):
     def can(self, permissions):
@@ -247,7 +261,26 @@ class Post(db.Model):
         target.body_html = bleach.linkify(bleach.clean(
             markdown(value, output_format='html'),
             tags=allowed_tags, strip=True))
+        
+    def to_json(self):
+        json_post = {
+            'url': url_for('api.get_post', id=self.id),
+            'body': self.body,
+            'body_html': self.body_html,
+            'timestamp': self.timestamp,
+            'author_url': url_for('api.get_user', id=self.author_id),
+            'comments_url': url_for('api.get_post_comments', id=self.id),
+            'comment_count': self.comments.count()
+        }
+        return json_post
 
+    @staticmethod
+    def from_json(json_post):
+        body = json_post.get('body')
+        if body is None or body == '':
+            raise ValidationError('post does not have a body')
+        return Post(body=body)
+    
 db.event.listen(Post.body, 'set', Post.on_changed_body)
 
 class Comment(db.Model):
