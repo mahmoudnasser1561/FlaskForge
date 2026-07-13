77b525e8e35548f8f9c2171241dd4e2198e4289f
Mahmoud <mahmoudnasser1561@gmail.com>
2026-07-13 22:59:19 +0300

    fix(cli): update Werkzeug profiler import and bypass CLI execution block
    - Migrates legacy 'werkzeug.contrib.profiler' to 'werkzeug.middleware.profiler'
    - Pops 'FLASK_RUN_FROM_CLI' from environment variables to prevent Flask from blocking 'app.run()' during profiling

--- app.py
+++ app.py
@@ -46,3 +46,23 @@ def test(coverage, test_names):
         COV.html_report(directory=covdir)
         print('HTML version: file://%s/index.html' % covdir)
         COV.erase()
+        
+@app.cli.command()
+@click.option('--length', default=25,
+              help='Number of functions to include in the profiler report.')
+@click.option('--profile-dir', default=None,
+              help='Directory where profiler data files are saved.')
+def profile(length, profile_dir):
+    """Start the application under the code profiler."""
+    import os 
+    from werkzeug.middleware.profiler import ProfilerMiddleware
+    
+    app.wsgi_app = ProfilerMiddleware(
+        app.wsgi_app, 
+        restrictions=[length],
+        profile_dir=profile_dir
+    )
+    
+    os.environ.pop("FLASK_RUN_FROM_CLI", None)
+    
+    app.run(debug=False)
