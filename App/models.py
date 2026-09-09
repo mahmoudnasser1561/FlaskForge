1ea90356773f88791e3af7487c60e6fedbae9a14
Mahmoud <mahmoudnasser1561@gmail.com>
2026-09-10 00:23:46 +0300

    fix(models): fix AnonymousUser.is_adminstrator typo

--- App/models.py
+++ App/models.py
@@ -269,7 +269,7 @@ class AnonymousUser(AnonymousUserMixin):
     def can(self, permissions):
         return False
     
-    def is_adminstrator(self):
+    def is_administrator(self):
         return False
     
 login_manager.anonymous_user = AnonymousUser
