12a13d6a071ef3b434cfb729913a2fa48a0eb411
Mahmoud <mahmoudnasser1561@gmail.com>
2026-09-10 14:53:05 +0300

    feat(notifications): redesign as bell dropdown, reuse pagination page

--- App/main/__init__.py
+++ App/main/__init__.py
@@ -4,7 +4,7 @@ from flask_login import current_user
 main = Blueprint('main', __name__)
 
 from . import views, errors
-from ..models import Permission
+from ..models import Permission, Notification
 
 @main.app_context_processor
 def inject_permissions():
@@ -14,6 +14,9 @@ def inject_permissions():
 def inject_notifications():
     if current_user.is_authenticated:
         count = current_user.notifications.filter_by(read=False).count()
+        recent = current_user.notifications.order_by(
+            Notification.timestamp.desc()).limit(5).all()
     else:
         count = 0
-    return dict(unread_notification_count=count)
+        recent = []
+    return dict(unread_notification_count=count, recent_notifications=recent)
