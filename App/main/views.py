236b40f4e9ad1a26ac41bc4d4231fa9dddb9881d
Mahmoud <mahmoudnasser1561@gmail.com>
2026-09-14 00:09:56 +0300

    Redesign edit post page: form-card component, centered layout

--- App/main/views.py
+++ App/main/views.py
@@ -147,7 +147,7 @@ def edit(id):
         return redirect(url_for('.post', id=post.id))
 
     form.body.data = post.body
-    return render_template('edit_post.html', form=form)
+    return render_template('edit_post.html', form=form, post=post)
     
 @main.route('/follow/<username>')
 @login_required
